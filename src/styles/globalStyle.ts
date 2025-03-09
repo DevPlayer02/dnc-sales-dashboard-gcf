6000481f2c1329d27fae370269c605c449e21af9
DevPlayer02 <contato.giovanicf@gmail.com>
2025-03-09 15:00:02 -0300

    Feat: Home Page Request

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -10,6 +10,7 @@ export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
     padding: 0;
     box-sizing: border-box;
     font-family: 'Inter', sans-serif;
+    width: 100%;
   }
 
   h1, h2, p, ul, li, figure {
@@ -23,5 +24,30 @@ export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
 
   .mb-2 {
     margin-bottom: ${pxToRem(32)};
+  }
+
+  .mb-3 {
+    margin-bottom: ${pxToRem(64)};
+  }
+
+  .skeleton-loading {
+    animation: skeletonLoading 2s infinite alternate;
+  }
+
+  @key-frames skeletonLoading {
+    from {
+      background-color: ${(props) => props.theme.appSkeletonFrom};
+    }
+    to {
+      background-color: ${(props) => props.theme.appSkeletonTo};
+    }
+  }
+
+  .skeleton-loading-mh-1 {
+    min-height: ${pxToRem(175)}
+  }
+
+  .skeleton-loading-mh-2 {
+    min-height: ${pxToRem(400)}
   }
 `
